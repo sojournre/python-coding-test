940533bb9c0078f3f907b7328dc9b728263b1757
sojournre <sojournre@gmail.com>
2022-06-19 22:40:22 +0900

    Python coding test study "invert binary tree"

---
+++ leetcode/LC_226_invert-binary-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+import collections
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        queue = collections.deque([root])
+
+        while queue:
+            node = queue.popleft()
+            # 부모 노드부터 하향식 스왑
+            if node:
+                node.left, node.right = node.right, node.left
+
+                queue.append(node.left)
+                queue.append(node.right)
+
+        return root
