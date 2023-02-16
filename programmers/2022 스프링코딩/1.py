cfb1fc21a26abdc36d6b37a8b4f9e64f830cf730
sojournre <sojournre@gmail.com>
2023-02-16 18:31:57 +0900

    프로그래머스 2023 스프링코딩 코딩테스트 다시 풀어보기

--- programmers/2022 스프링코딩/1.py
+++ programmers/2022 스프링코딩/1.py
@@ -1,24 +1,27 @@
 def solution(lotteries):
     answer = 0
-    a = list(enumerate(lotteries))
-    # for i, v in enumerate(lotteries):
-    #     print(i, v)
-    #     # print(v[0])
-    #     # print(v[1] + 1)
-    #     # var = v[1]
-    #     if var >= 100:
-    #         var = 100
-    #     lotteries.append(var)
-    # print(lotteries)
+    var = 0
+    # a = list(enumerate(lotteries))
+    for i, v in enumerate(lotteries):
+        v.append(i + 1)
+        print(i, v)
+        print(v[0])
+        print(v[1] + 1)
+        percent = v[0] / (v[1] + 1) * 100
+        print(percent)
+        if percent >= 100:
+            percent = 100
+        v.append(percent)
+    print(lotteries)
 
     # print(a)
     # print(a[1][1][2])
     # sorted_lotterires = sorted(a, key=lambda x: x[1][2])
-    sorted_lotterires = sorted(a, key=lambda x: (-x[1][0] / (x[1][1]+1) * 100, -x[1][2]))
+    sorted_lotterires = sorted(lotteries, key=lambda x: (-x[4], -x[2]))
     print(sorted_lotterires)
 
-    return sorted_lotterires[0][0] + 1
+    return sorted_lotterires[0][3]
 
 
-lotteries = [[50, 1, 50], [100, 199, 100], [2, 1, 500]]
+lotteries = [[50, 100, 50], [100, 199, 100], [2, 200, 500]]
 print(solution(lotteries))
--- programmers/2022 스프링코딩/2.py
+++ programmers/2022 스프링코딩/2.py
@@ -1,10 +1,12 @@
 def solution(grid):
+    visit = [[False for i in range(len(grid))] for j in range(len(grid[0]))]
+
     def dfs(i, j):
-        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '#':
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or visit[i][j] != '#':
             return False
 
-        grid[i][j] = '.'
-
+        # grid[i][j] = '.'
+        visit[i][j] = True
         # 상하좌우 대각 탐색
         dfs(i + 1, j)
         dfs(i - 1, j)
